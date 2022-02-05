ec541a6108858aad230f8864d6199335f646a319
FerRom1979 <fernando_0135@hotmail.com>
2022-02-05 16:14:41 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+/* **********menu*********** */
+;((d) => {
+  const $btnMenu = d.querySelector(".menu-btn")
+  const $menu = d.querySelector(".menu")
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none")
+    $btnMenu.lastElementChild.classList.toggle("none")
+    $menu.classList.toggle("is-active")
+  })
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false
+    $btnMenu.firstElementChild.classList.remove("none")
+    $btnMenu.lastElementChild.classList.add("none")
+    $menu.classList.remove("is-active")
+  })
+})(document)
+
+/* **********menu*********** */
+;((d) => {
+  const $form = d.querySelector(".contact-form")
+  const $loader = d.querySelector(".contact-form-loader")
+  const $response = d.querySelector(".contact-form-response")
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    $loader.classList.remove("none")
+    fetch("https://formsubmit.co/ajax/fernando_0135@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        location.hash = "thansk"
+        $form.reset()
+      })
+      .catch((err) => {
+        let message =
+          err.statusText ||
+          "An error occurred while submitting the form, please try again."
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`
+      })
+      .finally(() => {
+        $loader.classList.add("none")
+        setTimeout(() => {
+          location.hash = "#close"
+        }, 3000)
+      })
+  })
+})(document)
